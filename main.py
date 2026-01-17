35bd15ab542b24eb300affc151aec24852fe6107
Aaditya2910 <thakaraaditya2198@gmail.com>
2026-01-17 17:01:27 +0530

    Refactor project structure: remove old database, models, and routes; add new modular files for assessment and authentication

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routes import auth, questions, assess
-
-app = FastAPI()
-
-app.include_router(auth.router)
-app.include_router(questions.router)
-app.include_router(assess.router)
